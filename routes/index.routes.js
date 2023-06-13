dc0636505f0fe87c13438ea5bd1769fd751f76e1
Alejandro <alexsh882@gmail.com>
2023-06-13 16:58:10 -0300

    se cambió el nombre de la función delete a destroy por que generaba conflicto de nombres

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/', indexController.index)
 router.post('/', indexController.store)
 router.put('/', indexController.update)
-router.delete('/', indexController.delete)
+router.delete('/', indexController.destroy)
 
 
 
